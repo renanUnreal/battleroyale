2e57b38b9352b348de4649b6542c522b778b3595
renanUnreal <renanunreal@gmail.com>
2019-05-12 19:36:26 -0300

    Update MV_Aviao.cs

--- MV_Aviao.cs
+++ MV_Aviao.cs
@@ -1,22 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MV_Aviao : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public float tempo;
+    public GameObject AirDrop;
+    public int ctrlSpawn;
    
 
 
     void Start()
     {
-      
+        tempo = 0;
+        ctrlSpawn = 0; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * (-10) * Time.deltaTime);
-    }
+        tempo += Time.deltaTime;
+
+        
+
+        transform.Translate(Vector3.forward * (-30) * Time.deltaTime);
+
+        if (tempo >= 30)
+        {
+            Destroy(gameObject);
+
+        }
+       
+       
+            if (tempo > 6 && ctrlSpawn <= 0)
+            {
+                Instantiate(AirDrop, transform.position, transform.rotation);
+            ctrlSpawn = 1;
+            }
+        }
+    
 }
