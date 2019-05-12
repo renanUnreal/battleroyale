dc7d5983f86d35276386748f3978f3b0b79e9998
renanUnreal <renanunreal@gmail.com>
2019-05-12 18:43:43 -0300

    Add files via upload

---
+++ AirDrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirDrop : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Paraquedas;
+    public GameObject fumacaAzul;
+
+    void Start()
+    {
+        fumacaAzul.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        fumacaAzul.SetActive(true);
+        Paraquedas.SetActive(false);
+    }
+
+}
