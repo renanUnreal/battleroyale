eea5121fb9905f9d1fcdb76aa4539c12e99f290a
renanUnreal <renanunreal@gmail.com>
2019-05-14 00:39:27 -0300

    Add files via upload

---
+++ AnimacaoGaz.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaDanoGás : MonoBehaviour
+{
+    public Texture2D[] textura;
+    public float valor;
+    public int ValorInt;
+    public float t;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //esqueminha basico de efeito de dano do gás 
+        //criado por Marcos DS
+        t += Time.deltaTime;
+        if (t > 1)
+        {
+            ValorInt = Mathf.FloorToInt(valor = 10 * Random.value);
+            t = 0;
+        }
+        transform.GetComponent<Renderer>().material.mainTexture = textura[ValorInt];
+    }
+}
