1924a8a6b6faa7035e8e06ba01484804a739ce69
renanUnreal <renanunreal@gmail.com>
2019-05-14 01:03:07 -0300

    Update SafeZone.cs

--- SafeZone.cs
+++ SafeZone.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SafeZone : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static float DistanciaRelCentroSZ;
+    public Vector3 posiBorda;
+    public Vector3 posiCentroSZ;
     void Start()
     {
         
@@ -13,6 +16,40 @@ public class SafeZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Pegando as posições da borda da safezone
+        posiBorda = transform.position;
+        posiCentroSZ = GameObject.FindWithTag("CentroSafeZone").transform.position;
+
+        //  travando as variaveis para nao haver valores negativos
+        if (posiBorda.x < 0 )
+        {
+            posiBorda.x = -posiBorda.x;
+        }
+        if (posiBorda.z < 0)
+        {
+            posiBorda.z = -posiBorda.z;
+        }
+
+        if (posiCentroSZ.x < 0)
+        {
+            posiCentroSZ.x = -posiCentroSZ.x;
+        }
+        if (posiCentroSZ.z < 0)
+        {
+            posiCentroSZ.z = -posiCentroSZ.z;
+        }
+
+
+
+        // pegando a distancia do centro em relação a borda da safezone ponto (A) e ponto (B)
+        DistanciaRelCentroSZ = Vector3.Distance(posiBorda, posiCentroSZ);
+
+
+       
+    }
+
+    private void OnGUI()
+    {
+        GUI.TextField(new Rect(10, 10, 130, 20), "Distancia da Safezone: " + posiCentroSZ);
     }
 }
